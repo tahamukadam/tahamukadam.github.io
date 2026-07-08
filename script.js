7577a075034b741961faea192cef6647ca91e2ea
tahamukadam <tahamukadam780@gmail.com>
2026-07-08 23:17:49 +0530

    new git page

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-// Simple scroll animation logging
-window.addEventListener("scroll", () => {
-    console.log("Scrolling...");
+const text = [
+    "Full Stack Developer",
+    "Cloud Computing Learner",
+    "Python Developer",
+    "Web Designer"
+];
+
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function type() {
+
+    current = text[i];
+
+    if (!isDeleting) {
+        document.getElementById("typing").textContent =
+            current.substring(0, j++);
+    } else {
+        document.getElementById("typing").textContent =
+            current.substring(0, j--);
+    }
+
+    if (!isDeleting && j === current.length + 1) {
+        isDeleting = true;
+        setTimeout(type, 1000);
+        return;
+    }
+
+    if (isDeleting && j === 0) {
+        isDeleting = false;
+        i = (i + 1) % text.length;
+    }
+
+    setTimeout(type, isDeleting ? 60 : 120);
+}
+
+type();
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("show");
 });
